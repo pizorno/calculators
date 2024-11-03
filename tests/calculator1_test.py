f6c21d0506d44e92ea431c76aae5359a13cdeb66
Eduardo Pizorno <pizorno@gmail.com>
2024-11-03 08:58:46 -0300

    Implementando teste de execeções

--- tests/calculator1_test.py
+++ tests/calculator1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from calculators.app.calculator1 import Calculator1
 
 class MockRequest:
@@ -14,3 +15,10 @@ def test_calculate():
     assert "result" in response["data"]
     assert response["data"]["result"] == 17.39
     assert response["data"]["Calculator"] == 1
+
+def test_calculate_body_error():
+     mock_request = MockRequest(body={"something": 5})
+     calculator1 = Calculator1()
+     with raises(Exception) as excinfo:
+        calculator1.calculate(mock_request)
+     assert str(excinfo.value) == "body mal formatado!"
