e59407b6d183c9bbe99e8c60683eec4e25fa23d4
Eduardo Pizorno <pizorno@gmail.com>
2024-11-03 08:40:14 -0300

    Teste da função calculate

--- tests/calculator1_test.py
+++ tests/calculator1_test.py
@@ -1,5 +1,16 @@
-def test_something():
-    pass
+from typing import Dict
+from calculators.app.calculator1 import Calculator1
 
-def test_other_something():
-    pass
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest(body={"number": 5})
+    calculator1 = Calculator1()
+    response = calculator1.calculate(mock_request)
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "result" in response["data"]
+    assert response["data"]["result"] == 17.39
+    assert response["data"]["Calculator"] == 1
