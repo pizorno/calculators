6b4c9ea9e089984029195f307bae55eac8fbcba1
Eduardo Pizorno <pizorno@gmail.com>
2024-11-04 17:06:43 -0300

    Ajustando testes da calculadora 2

--- tests/calculator2_test.py
+++ tests/calculator2_test.py
@@ -1,15 +1,28 @@
-from typing import Dict
+from typing import Dict, List
 from calculators.app.calculator2 import Calculator2
 from calculators.drivers.numpy_handler import NumpyHandler
+from calculators.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        return 3
+
+def test_calculate_integration():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
     driver = NumpyHandler()
     calculator2 = Calculator2(driver)
     formated_response = calculator2.calculate(mock_request)
     assert isinstance(formated_response, dict)
     assert formated_response == {"data": {"Calculator": 2, "result": 0.08}}
+
+def test_calculate_integration():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+    driver = MockDriverHandler()
+    calculator2 = Calculator2(driver)
+    formated_response = calculator2.calculate(mock_request)
+    assert isinstance(formated_response, dict)
+    assert formated_response == {"data": {"Calculator": 2, "result": 0.33}}
