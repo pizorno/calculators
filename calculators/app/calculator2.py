4a5c24d84784271b52ae70d7c8641e93f14ba4ae
Eduardo Pizorno <pizorno@gmail.com>
2024-11-04 16:55:30 -0300

    Implementando Interface

--- calculators/app/calculator2.py
+++ calculators/app/calculator2.py
@@ -1,8 +1,11 @@
 from typing import Dict, List
 from flask import request as FlaskRequest
-from calculators.drivers.numpy_handler import NumpyHandler
+from calculators.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class Calculator2:
+    def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+        self.__driver_handler = driver_handler
+
     def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
@@ -18,9 +21,8 @@ class Calculator2:
         return input_data
 
     def __process_data(self, input_data: List[float]) -> float:
-        numpy_handler = NumpyHandler()
         first_process = [(num * 11) ** 0.95 for num in input_data]
-        result = numpy_handler.standard_derivation(first_process)
+        result = self.__driver_handler.standard_derivation(first_process)
         return 1/result
 
     def __format_response(self, calculated_number: float) -> Dict:
--- tests/calculator2_test.py
+++ tests/calculator2_test.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from calculators.app.calculator2 import Calculator2
+from calculators.drivers.numpy_handler import NumpyHandler
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -7,7 +8,8 @@ class MockRequest:
 
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
-    calculator2 = Calculator2()
+    driver = NumpyHandler()
+    calculator2 = Calculator2(driver)
     formated_response = calculator2.calculate(mock_request)
     assert isinstance(formated_response, dict)
     assert formated_response == {"data": {"Calculator": 2, "result": 0.08}}
