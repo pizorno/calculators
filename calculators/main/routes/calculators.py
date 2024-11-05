9e0877a49ab863939bab81ed4f51ff988feb3fb2
Eduardo Pizorno <pizorno@gmail.com>
2024-11-05 09:26:12 -0300

    Adicionando rota para calculadora 2

--- calculators/main/routes/calculators.py
+++ calculators/main/routes/calculators.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, jsonify, request
 from calculators.app.calculator1 import Calculator1
+from calculators.app.calculator1 import Calculator2
+from calculators.drivers.numpy_handler import NumpyHandler
 
 calc_route_bp = Blueprint("calc_routes", __name__)
+
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calc = Calculator1()
+    response = calc.calculate(request)
+    return jsonify(response), 200
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpy_handler = NumpyHandler()
+    calc = Calculator2(numpy_handler)
     response = calc.calculate(request)
     return jsonify(response), 200
